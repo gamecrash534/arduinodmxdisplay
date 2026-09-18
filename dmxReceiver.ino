12d7748e76117086da95694a2de54acdacc37653
game.crash <73080127+gamecrash534@users.noreply.github.com>
2026-09-18 17:24:05 +0200

    slight code optimizations

--- dmxReceiver.ino
+++ dmxReceiver.ino
@@ -5,48 +5,41 @@
 #define SLAVE_CHANNELS 512
 #define RXEN_PIN 2
 
-DMX_Slave dmx_slave (SLAVE_CHANNELS, RXEN_PIN);
+DMX_Slave dmx_slave(SLAVE_CHANNELS, RXEN_PIN);
 Waveshare_LCD1602_RGB lcd(16, 2);
 
-uint8_t lastRead[512];
+uint8_t lastRead[SLAVE_CHANNELS];
 
 void setup() {
-  dmx_slave.enable();
-  dmx_slave.setStartAddress(1);
+    dmx_slave.enable();
+    dmx_slave.setStartAddress(1);
 
-  lcd.init();
-  lcd.setCursor(0, 0);
-  write0("no new value yet");
+    lcd.init();
+    lcd.setCursor(0, 0);
+    lcd.send_string("no values yet");
 }
 
 void loop() {
-  //noInterrupts();
-  for (int i = 1; i <= SLAVE_CHANNELS; i++) {
-    uint8_t v = dmx_slave.getChannelValue(i);
+    for (uint16_t ch = 1; ch <= SLAVE_CHANNELS; ch++) {
+        uint8_t val = dmx_slave.getChannelValue(ch);
 
-    uint8_t lv = lastRead[i - 1];
-    if (v == lv) continue;
+        if (val == lastRead[ch - 1]) continue;
 
-    lastRead[i - 1] = v;
+        lastRead[ch - 1] = val;
 
-    String s = String("Ch" + prependNullsToNum(i) + ": " + prependNullsToNum(v));
-    write0(s.c_str());
-  }
-  //interrupts();
+        char buf[11];
+        snprintf(buf, sizeof(buf), "Ch%03u: %03u", ch, val);
+
+        write0(buf);
+    }
 }
 
 void write0(String str) {
-  lcd.setCursor(0, 0);
-  lcd.send_string(str.c_str());
+    lcd.setCursor(0, 0);
+    lcd.send_string(str.c_str());
 }
 
 void write1(String str) {
-  lcd.setCursor(0, 1);
-  lcd.send_string(str.c_str());
-}
-
-String prependNullsToNum(int val) {
-  if (val > 99) return String(val);
-  else if (val > 9) return "0" + String(val);
-  else return "00" + String(val);
+    lcd.setCursor(0, 1);
+    lcd.send_string(str.c_str());
 }
